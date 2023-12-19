884f7c7da0c31ff64fc5479b6b178afaa1a72e70
Nsanga Mercure Mekinda <mercuremekinda@gmail.com>
2023-12-19 12:56:03 +0100

    first commit

--- src/views/Recap/index.js
+++ src/views/Recap/index.js
@@ -79,7 +79,7 @@ const Recap = ({ selectedDrinks, quantityValues, type, selectedFoods }) => {
 
             // (Optionnel) Rediriger l'utilisateur vers WhatsApp
             // Remplacez le lien ci-dessous par votre lien WhatsApp
-            window.location.href = 'https://api.whatsapp.com';
+            window.location.href = 'whatsapp://app';
 
             // Fermer la modal
             handleClose()
